8e29a627109ee418a72c706e26cf0ad37b6d083d
AlirezaOpmc <alirezaopmc@gmail.com>
2023-02-24 19:27:44 +0330

    fix

--- install.sh
+++ install.sh
@@ -8,22 +8,12 @@ then
   read domain
 fi
 
-# Certification
+# Update
 apt update && apt upgrade -y
-apt install curl socat -y
-curl https://get.acme.sh | sh
-~/.acme.sh/acme.sh --set-default-ca --server letsencrypt
-~/.acme.sh/acme.sh --register-account -m xxxx@xxxx.com
-~/.acme.sh/acme.sh --issue -d $domain --standalone
-~/.acme.sh/acme.sh --installcert -d $domain --key-file /root/private.key --fullchain-file /root/cert.cer
-cp /root/cert.cer /pb
-cp /root/private.key /pv
+
+# Certification
+bash ./src/make_cert.sh
 
 # X-UI
 bash ./make-xui-data.sh
 bash <(curl -Ls https://raw.githubusercontent.com/FranzKafkaYu/x-ui/master/install_en.sh) < xui.data
-
-# Nginx
-apt install nginx -y
-cp ./nginx.conf /etc/nginx/nginx.conf
-nginx -s reload
---
+++ src/make_cert.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+domain=$1
+
+apt install curl socat -y
+curl https://get.acme.sh | sh
+~/.acme.sh/acme.sh --set-default-ca --server letsencrypt
+~/.acme.sh/acme.sh --register-account -m xxxx@xxxx.com
+~/.acme.sh/acme.sh --issue -d $domain --standalone
+~/.acme.sh/acme.sh --installcert -d $domain --key-file /root/private.key --fullchain-file /root/cert.cer
+
+cp /root/cert.cer /pb
+cp /root/private.key /pv
