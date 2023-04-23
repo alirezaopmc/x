1bd96d337751cc133ad7efa5078c25fe4d788e90
AlirezaOpmc <alirezaopmc@gmail.com>
2023-04-23 16:40:35 +0330

    New cert method

--- src/make_cert.sh
+++ src/make_cert.sh
@@ -7,7 +7,5 @@ curl https://get.acme.sh | sh
 ~/.acme.sh/acme.sh --set-default-ca --server letsencrypt
 ~/.acme.sh/acme.sh --register-account -m xxxx@xxxx.com
 ~/.acme.sh/acme.sh --issue -d $domain --standalone
-~/.acme.sh/acme.sh --installcert -d $domain --key-file /root/private.key --fullchain-file /root/cert.cer
-
-cp /root/cert.cer /pb
-cp /root/private.key /pv
+mkdir -p /root/certs/$domain
+~/.acme.sh/acme.sh --installcert -d $domain --key-file /root/certs/$domain/private.key --fullchain-file /root/certs/$domain/cert.cer
